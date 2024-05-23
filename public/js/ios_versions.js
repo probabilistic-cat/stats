c2b80248490efa5c280784aba2784dfda4022a38
Andrey <andrewenka@gmail.com>
2024-05-23 14:28:14 +0300

    iOS versions page data.

--- public/js/ios_versions.js
+++ public/js/ios_versions.js
@@ -9,6 +9,49 @@ function selectSubcategory() {
     });
 }
 
+function calcVersionDivWidth() {
+    $('.content div.data div.month div.versions').each(function() {
+        let versionsDivWidth = parseInt($(this).first().width());
+
+        let biggestWidth = 0;
+        let classes = '';
+        let totalWidth = 0;
+        $(this).find('div.version').each(function() {
+            let version = $(this).attr('data-version');
+            let percent = parseFloat($(this).attr('data-percent'));
+            let width = Math.floor(versionsDivWidth / 100 * percent);
+            totalWidth += width;
+            if (biggestWidth < width) {
+                let classlist = $(this).attr('class').split(/\s+/);
+                classes = '.' + classlist.join('.');
+                biggestWidth = width;
+            }
+
+            $(this).css('width', width + 'px');
+            if (110 <= width) {
+                $(this).html('iOS ' + version + ' - ' + percent + '%');
+            } else if (50 <= width || (percent < 10 && 40 <= width)) {
+                $(this).html(percent + '%');
+            }
+        });
+
+        if (totalWidth < versionsDivWidth) {
+            let widthDiff = versionsDivWidth - totalWidth;
+            let biggestDiv = $(this).find('div' + classes);
+            let biggestDivWidth = biggestWidth + widthDiff;
+            biggestDiv.css('width', biggestDivWidth + 'px');
+        }
+
+        // console.log('versionsDivWidth ' + versionsDivWidth);
+    });
+}
+
+
 $(document).ready(function() {
     selectSubcategory();
+    calcVersionDivWidth();
+});
+
+$(window).resize(function() {
+    calcVersionDivWidth();
 });
--- src/Controller/IosVersionsController.php
+++ src/Controller/IosVersionsController.php
@@ -5,12 +5,41 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class IosVersionsController extends AbstractController
 {
     #[Route('/', name: 'app_ios_versions')]
-    public function all(): Response
+    public function ios_versions(SerializerInterface $serializer): Response
     {
-        return $this->render('ios_versions.html.twig');
+        $rawData = $serializer->decode(file_get_contents(
+            __DIR__.'/../Data/ios_version-ww-monthly-201706-202404.csv'),
+            'csv'
+        );
+
+        $result = [];
+        foreach ($rawData as $rawMonthData) {
+            $date = $rawMonthData['Date'];
+            $monthPercentOther = $rawMonthData['Other'];
+            unset($rawMonthData['Date'], $rawMonthData['Other']);
+            foreach ($rawMonthData as $majorVersion => $dataByMinorVersion) {
+                $majorVersion = mb_substr($majorVersion, mb_strrpos($majorVersion, ' ') + 1);
+                $majorVersionPercent = 0;
+                foreach ($dataByMinorVersion as $minorVersion => $percent) {
+                    $version = $majorVersion.'.'.$minorVersion;
+                    $percent = (float)$percent;
+//                    $result[$date][$version] = $percent;
+                    $majorVersionPercent += $percent;
+                }
+                $result[$date][$majorVersion] = $majorVersionPercent;
+            }
+            ksort($result[$date]);
+            $result[$date] = ['other' => $monthPercentOther] + $result[$date];
+        }
+        krsort($result);
+
+        return $this->render('ios_versions.html.twig', [
+            'data' => $result,
+        ]);
     }
 }
