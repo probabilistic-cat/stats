2756dbb038deb348b84a382a722f69d5011f332d
Andrey <andrewenka@gmail.com>
2024-05-31 21:51:39 +0300

    Sort by reference.

--- src/Repository/Data/Data.php
+++ src/Repository/Data/Data.php
@@ -35,24 +35,27 @@ class Data
     }
 
     public function sort(): void {
+        usort($this->monthDatas, static fn (MonthData $a, MonthData $b): int => $b->date <=> $a->date);
+
+        $reference = $this->getSortVersionsReference($this->monthDatas[0]);
+
         foreach ($this->monthDatas as $monthIndex => $monthData) {
             if ($this->hasMinor()) {
                 foreach ($monthData->versions as $versionIndex => $version) {
                     if (count($version->minorVersions) > 0) {
                         usort(
                             $this->monthDatas[$monthIndex]->versions[$versionIndex]->minorVersions,
-                            fn (Version $a, Version $b): int => $this->sortVersions($a, $b),
+                            static fn (Version $a, Version $b): int => $a->version <=> $b->version,
                         );
                     }
                 }
             }
+
             usort(
                 $this->monthDatas[$monthIndex]->versions,
-                fn (Version $a, Version $b): int => $this->sortVersions($a, $b),
+                static fn (Version $a, Version $b): int => $reference[$a->version] <=> $reference[$b->version],
             );
         }
-
-        usort($this->monthDatas, static fn (MonthData $a, MonthData $b): int => $b->date <=> $a->date);
     }
 
     public function setColors(): void {
@@ -72,7 +75,7 @@ class Data
      * @return array{string: string}
      */
     private function getColorsByVersion(): array {
-        $colorsByVersion = $this->getAllVersions();
+        $colorsByVersion = array_fill_keys($this->getAllVersionsSortedAsc(), null);
         $colorsByVersion = array_replace($colorsByVersion, $this->profile->customColorsByVersion);
 
         $colorIndex = 0;
@@ -113,7 +116,7 @@ class Data
     /**
      * @return array{int|string: null}
      */
-    private function getAllVersions(): array {
+    private function getAllVersionsSortedAsc(): array {
         $allVersions = [];
         foreach ($this->monthDatas as $monthData) {
             foreach ($monthData->versions as $version) {
@@ -125,29 +128,47 @@ class Data
         }
         ksort($allVersions, SORT_NATURAL);
 
-        return $allVersions;
+        return array_keys($allVersions);
     }
 
     private function isVersionNameMajor(string $versionName): bool {
         return mb_strpos($versionName, $this->profile->versionSeparator) === false;
     }
 
-    private function sortVersions(Version $a, Version $b): int {
-        if (in_array($a->version, BaseProfile::VERSIONS_OTHER, true)) {
-            return -1;
-        }
-        if (in_array($b->version, BaseProfile::VERSIONS_OTHER, true)) {
-            return 1;
+    /**
+     * @return array{string: int}
+     */
+    private function getSortVersionsReference(MonthData $lastMontData): array {
+        $reference = [];
+        $allVersions = $this->getAllVersionsSortedAsc();
+        $priority = 0;
+
+        $versionsOther = BaseProfile::VERSIONS_OTHER;
+        rsort($versionsOther);
+        foreach (BaseProfile::VERSIONS_OTHER as $versionName) {
+            $reference[$versionName] = $priority;
+            $priority++;
         }
 
         if ($this->profile->sort === BaseProfile::SORT_PERCENT_ASC) {
-            return $a->percent <=> $b->percent;
+            $lastMonthVersions = $lastMontData->versions;
+            usort($lastMonthVersions, static fn (Version $a, Version $b): int => $b->percent <=> $a->percent);
+            $percentPriority = count($allVersions) + count(BaseProfile::VERSIONS_OTHER);
+            foreach ($lastMonthVersions as $version) {
+                if (!in_array($version->version, BaseProfile::VERSIONS_OTHER, true)) {
+                    $reference[$version->version] = $percentPriority;
+                    $percentPriority--;
+                }
+            }
         }
 
-        if ($this->profile->sort === BaseProfile::SORT_VERSION_ASC) {
-            return $a->version <=> $b->version;
+        foreach ($allVersions as $versionName) {
+            if (!array_key_exists($versionName, $reference)) {
+                $reference[$versionName] = $priority;
+                $priority++;
+            }
         }
 
-        return $a->version <=> $b->version;
+        return $reference;
     }
 }
