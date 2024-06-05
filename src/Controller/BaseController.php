4b7fb91df496df908b45107eb55dc888bd9428d3
Andrey <andrewenka@gmail.com>
2024-06-05 18:56:05 +0300

    Data cached.

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,23 +4,38 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\Consts;
+use App\Repository\Data\Data;
 use App\Repository\Data\DataFileDecoder;
 use App\Repository\Profile\BaseProfile;
+use Psr\Cache\CacheItemInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class BaseController extends AbstractController
 {
+    private const int DATA_CACHE_EXPIRATION_TIME = Consts::SECONDS_IN_YEAR;
+
     public function __construct(
         private readonly DataFileDecoder $decoder,
+        private readonly CacheInterface $cache,
     ) {}
 
     protected function getResponse(BaseProfile $profile, string $subcategory, string $categoryLink): Response {
-        $filepath = $profile->getFilePathBySubcategory($subcategory);
-        $data = $this->decoder->decode(profile: $profile, filepath: $filepath);
-        $data->filterOutZeroPercentVersions();
-        $data->sort();
-        $data->setColors();
+        $cacheKey = self::getDataCacheKey(profile: $profile, subcategory: $subcategory);
+
+        $data = $this->cache->get($cacheKey, function (CacheItemInterface $cacheItem) use ($profile, $subcategory) {
+            $cacheItem->expiresAfter(self::DATA_CACHE_EXPIRATION_TIME);
+
+            $filepath = $profile->getFilePathBySubcategory($subcategory);
+            $data = $this->decoder->decode(profile: $profile, filepath: $filepath);
+            $data->filterOutZeroPercentVersions();
+            $data->sort();
+            $data->setColors();
+            return $data;
+        });
+        assert($data instanceof Data);
 
         return $this->render('content.html.twig', [
             'category' => $profile->category,
@@ -33,5 +48,9 @@ class BaseController extends AbstractController
             'data' => $data,
             'hasMinor' => $data->hasMinor(),
         ]);
+    }
+
+    private static function getDataCacheKey(BaseProfile $profile, string $subcategory): string {
+        return mb_strtolower(str_replace(' ', '_', 'data_'.$profile->category.'_'.$subcategory));
     }
 }
--- src/Repository/Consts.php
+++ src/Repository/Consts.php
@@ -7,4 +7,11 @@ namespace App\Repository;
 class Consts
 {
     public const string DIR = __DIR__;
+
+    public const int SECONDS_IN_MINUTE = 60;
+    public const int SECONDS_IN_HOUR = self::SECONDS_IN_MINUTE * 60;
+    public const int SECONDS_IN_DAY = self::SECONDS_IN_HOUR * 24;
+    public const int SECONDS_IN_WEEK = self::SECONDS_IN_DAY * 7;
+    public const int SECONDS_IN_MONTH = self::SECONDS_IN_DAY * 31;
+    public const int SECONDS_IN_YEAR = self::SECONDS_IN_DAY * 366;
 }
